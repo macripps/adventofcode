a51903fbe929dff8d47d3120c9e610bd0d51774a
Mike Cripps <mike.a.cripps@gmail.com>
2020-12-17 09:54:22 +0000

    Prep day 18

---
+++ src/main/scala/aoc2020/Day18.scala
@@ -0,0 +1,20 @@
+package aoc2020
+
+import aoc.Day
+
+class Day18 extends Day {
+  override def year: Int = 2020
+  override def day: Int = 18
+
+  override def part1(input: Array[String]): String = {
+    ""
+  }
+
+  override def part2(input: Array[String]): String = {
+    ""
+  }
+}
+
+object Day18 {
+  def apply() = new Day18()
+}
