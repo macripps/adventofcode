169240c502c207c2ec96c8c02227321fb27aa1f3
Mike Cripps <mike.a.cripps@gmail.com>
2020-12-19 11:31:09 +0000

    Remove unnecessary variables

--- src/main/scala/aoc2020/Day19.scala
+++ src/main/scala/aoc2020/Day19.scala
@@ -3,54 +3,52 @@ package aoc2020
 import aoc.Day
 import aoc2020.Day19._
 
+import scala.collection.mutable
+
 class Day19 extends Day {
   override def year: Int = 2020
   override def day: Int = 19
 
   override def part1(input: Array[String]): String = {
-    val rules = input.take(input.indexOf("")).sortBy{ f => f.take(f.indexOf(':')).toInt }
+    val rules = buildRules(input.take(input.indexOf("")))
     val possibilities = buildValid(rules, 0, 0)
     val strings = input.drop(input.indexOf("") + 1)
-    strings.count{ s =>  possibilities.r.matches(s) }.toString
+    strings.count { s => possibilities.r.matches(s) }.toString
   }
 
   override def part2(input: Array[String]): String = {
-    val rules = input.take(input.indexOf("")).sortBy{ f => f.take(f.indexOf(':')).toInt }
-    rules(8) = "8: 42 | 42 8"
-    rules(11) = "11: 42 31 | 42 11 31"
-    val strings = input.drop(input.indexOf("") + 1)
+    val rules = buildRules(input.take(input.indexOf("")) ++ Seq("8: 42 | 42 8", "11: 42 31 | 42 11 31"))
     // 5 is empirically determined to be sufficient
     val possibilities = buildValid(rules, 0, 5)
-    strings.count{ s =>  possibilities.r.matches(s) }.toString
+    val strings = input.drop(input.indexOf("") + 1)
+    strings.count { s => possibilities.r.matches(s) }.toString
+  }
+
+  def buildRules(input: Array[String]): Map[Int, String] = {
+    input.map { r => r.take(r.indexOf(':')).toInt -> r.drop(r.indexOf(' ') + 1) }.toMap
   }
 }
 
 object Day19 {
   def apply() = new Day19()
 
-  def buildValid(rules: Array[String], num: Int, depth: Int): String = {
-    val ruleToMatch = rules(num).drop(rules(num).indexOf(" ") + 1)
-    val output = new StringBuilder("")
-    val subRules = ruleToMatch.split(" \\| ")
-    val out = subRules.map { r =>
+  def buildValid(rules: Map[Int, String], num: Int, depth: Int): String = {
+    val out = rules(num).split(" \\| ").map { r =>
       if (r.startsWith("\"") && r.endsWith("\"")) {
         r.drop(1).dropRight(1)
       } else {
-        val rules2 = r.split(" ")
-        rules2.flatMap { ru =>
+        r.split(" ").flatMap { ru =>
           val ruleNum = ru.toInt
           if (ruleNum != num || depth > 0) {
-            buildValid(rules, ru.toInt, depth - 1)
+            buildValid(rules, ru.toInt, if (ruleNum == num) depth - 1 else depth)
           } else {
             ""
           }
         }.mkString("")
       }
     }
-    val res = if (out.length > 1) {
+    if (out.length > 1) {
       out.mkString("(", "|", ")")
     } else out.mkString("")
-    output.append(res)
-    output.toString()
   }
 }
--- src/test/scala/aoc2020/Day19Test.scala
+++ src/test/scala/aoc2020/Day19Test.scala
@@ -5,13 +5,13 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day19Test extends AnyFunSuite {
 
   test("example") {
-    val rules = Array[String](
-      "0: 4 1 5",
-      "1: 2 3 | 3 2",
-      "2: 4 4 | 5 5",
-      "3: 4 5 | 5 4",
-      "4: \"a\"",
-      "5: \"b\"",
+    val rules = Map[Int, String](
+      0 -> "4 1 5",
+      1 -> "2 3 | 3 2",
+      2 -> "4 4 | 5 5",
+      3 -> "4 5 | 5 4",
+      4 -> "\"a\"",
+      5 -> "\"b\"",
     )
     val reg = Day19.buildValid(rules, 0, 0).r
     println(reg)
