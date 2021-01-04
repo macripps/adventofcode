d44d2ead0a20711b34c70f2225ebf304fca14f3d
Mike Cripps <mike.a.cripps@gmail.com>
2021-01-04 23:43:45 +0000

    2016 Day 3

---
+++ src/main/scala/aoc2016/Day3.scala
@@ -0,0 +1,26 @@
+package aoc2016
+
+import aoc.Day
+
+class Day3 extends Day(2016, 3) {
+  override def part1: String = {
+    input.count { l =>
+      val a = Seq(l.substring(2, 5).replace(" ", ""), l.substring(7, 10).replace(" ", ""), l.substring(12, 15).replace(" ", "")).map(_.toInt).sorted
+      a(0) + a(1) > a(2)
+    }.toString
+  }
+
+  override def part2: String = {
+    input.grouped(3).flatMap { l =>
+      Seq(
+        Seq(l(0).substring(2, 5).replace(" ", ""), l(1).substring(2, 5).replace(" ", ""), l(2).substring(2, 5).replace(" ", "")).map(_.toInt).sorted,
+        Seq(l(0).substring(7, 10).replace(" ", ""), l(1).substring(7, 10).replace(" ", ""), l(2).substring(7, 10).replace(" ", "")).map(_.toInt).sorted,
+        Seq(l(0).substring(12, 15).replace(" ", ""), l(1).substring(12, 15).replace(" ", ""), l(2).substring(12, 15).replace(" ", "")).map(_.toInt).sorted
+      )
+    }.count { a => a(0) + a(1) > a(2) }.toString
+  }
+}
+
+object Day3 {
+  def apply() = new Day3()
+}
