caa35cc49bbc5afc19682e20f064eb32d34969c1
Mike Cripps <mike.a.cripps@gmail.com>
2021-12-02 06:03:42 +0000

    Minor refactor

--- src/main/scala/aoc2021/Day2.scala
+++ src/main/scala/aoc2021/Day2.scala
@@ -4,17 +4,14 @@ import aoc.Day
 
 class Day2 extends Day(2021, 2) {
   override def part1(input: Array[String]): String = {
-    var hPos = 0
-    var vPos = 0
-    input.foreach { l =>
-      val cmd = l.split(" ")
-      cmd(0) match {
-        case "forward" => hPos += cmd(1).toInt
-        case "down" => vPos += cmd(1).toInt
-        case "up" => vPos -= cmd(1).toInt
-      }
-    }
-    (hPos * vPos).toString
+    val pos = input.map { l =>
+      val n = l.split(' ')(1).toInt
+      if (l.startsWith("forward")) (n, 0)
+      else if (l.startsWith("down")) (0, n)
+      else if (l.startsWith("up")) (0, -n)
+      else (0,0)
+    }.reduceLeft[(Int,Int)]((x: (Int, Int), y: (Int, Int)) => (x._1 + y._1, x._2 + y._2))
+    (pos._1 * pos._2).toString
   }
 
   override def part2(input: Array[String]): String = {
---
+++ src/main/scala/aoc2021/Day3.scala
@@ -0,0 +1,17 @@
+package aoc2021
+
+import aoc.Day
+
+class Day3 extends Day(2021, 3) {
+  override def part1(input: Array[String]): String = {
+    ""
+  }
+
+  override def part2(input: Array[String]): String = {
+    ""
+  }
+}
+
+object Day3 {
+  def apply() = new Day3()
+}
