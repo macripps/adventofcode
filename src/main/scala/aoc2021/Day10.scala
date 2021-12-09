7d910fb204cae25a14a85cdfac3e8ed7c6d0c3d7
Mike Cripps <mike.a.cripps@gmail.com>
2021-12-09 09:23:35 +0000

    Day 10 prep

---
+++ src/main/scala/aoc2021/Day10.scala
@@ -0,0 +1,17 @@
+package aoc2021
+
+import aoc.Day
+
+class Day10 extends Day(2021, 10) {
+  override def part1(input: Array[String]): String = {
+    ""
+  }
+
+  override def part2(input: Array[String]): String = {
+    ""
+  }
+}
+
+object Day10 {
+  def apply() = new Day10
+}
