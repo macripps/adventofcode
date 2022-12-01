5691780bec1f8e7054a01fc139161dc702302daa
Mike Cripps <mike.a.cripps@gmail.com>
2022-12-01 10:21:29 +0000

    2022 Day 1

---
+++ src/main/scala/aoc2022/Day1.scala
@@ -0,0 +1,13 @@
+package aoc2022
+
+class Day1 extends aoc.Day(2022, 1) {
+  override def part1(input: Array[String]): String = super.inputGroups(input).map(x => x.map(_.toInt).sum).max.toString
+
+  override def part2(input: Array[String]): String = {
+    super.inputGroups(input).map(x => x.map(_.toInt).sum).toSeq.sorted.takeRight(3).sum.toString
+  }
+}
+
+object Day1 {
+  def apply() = new Day1()
+}
