db860f2b35b5d4bf89407aa2a8f8bc97bc4bdc77
Mike Cripps <mike.a.cripps@gmail.com>
2022-12-03 10:22:31 +0000

    Day 3

--- src/main/scala/aoc2022/Day3.scala
+++ src/main/scala/aoc2022/Day3.scala
@@ -1,9 +1,39 @@
 package aoc2022
 
 class Day3 extends aoc.Day(2022, 3) {
-  override def part1(input: Array[String]): String = ???
+  val test = Array(
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw"
+  )
+  override def part1(input: Array[String]): String = {
+    input.map { line =>
+      val (left, right) = line.splitAt(line.length/2)
+      left.filter {
+        l: Char => right.contains(l)
+      }.toSet.map(toPriority).sum
+    }.sum.toString
 
-  override def part2(input: Array[String]): String = ???
+  }
+
+  override def part2(input: Array[String]): String = {
+    input.grouped(3).map { lines =>
+      lines.head.filter { c =>
+        lines.tail.forall(l => l.contains(c))
+      }.toSet.map(toPriority).sum
+    }.sum.toString
+  }
+
+  private[this] def toPriority(p: Char): Int = {
+    if (p >= 'a' && p <= 'z') {
+      p - '`'
+    } else if (p >= 'A' && p <= 'Z') {
+      p + 26 - '@'
+    } else 0
+  }
 }
 
 object Day3 {
