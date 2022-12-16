b448c6a44980b4883fd3aa32fdce20610456512c
Mike Cripps <mike.a.cripps@gmail.com>
2022-12-16 20:21:47 +0000

    Day 17 prep

---
+++ src/main/scala/aoc2022/Day17.scala
@@ -0,0 +1,11 @@
+package aoc2022
+
+class Day17 extends aoc.Day(2022, 17) {
+  override def part1(input: Array[String]): Any = "???"
+
+  override def part2(input: Array[String]): Any = "???"
+}
+
+object Day17 {
+  def apply() = new Day17
+}
