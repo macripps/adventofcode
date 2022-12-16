5a857c28959413f4bea75351fd76670a598ec4ef
Mike Cripps <mike.a.cripps@gmail.com>
2022-12-16 20:19:47 +0000

    Day 16

--- src/main/scala/aoc2022/Day15.scala
+++ src/main/scala/aoc2022/Day15.scala
@@ -1,13 +1,141 @@
 package aoc2022
 
+import scala.collection.mutable
+import Day15._
+
+import scala.util.control.Breaks.{break, breakable}
+
 class Day15 extends aoc.Day(2022, 15) {
   override def part1(input: Array[String]): Any = {
-    ???
+    val sensors = mutable.Set[aoc.Point]()
+    val beacons = mutable.Set[aoc.Point]()
+    var minX = Integer.MAX_VALUE
+    var maxX = Integer.MIN_VALUE
+    val sensorBeaconPairs = mutable.Set[(aoc.Point, aoc.Point)]()
+    input.foreach {
+      case pLine(sX, sY, bX, bY) => {
+        val sensor = aoc.Point(sX.toInt, sY.toInt)
+        val beacon = aoc.Point(bX.toInt, bY.toInt)
+        sensors += sensor
+        beacons += beacon
+        sensorBeaconPairs += ((sensor, beacon))
+        val d = sensor.manhattanDistanceTo(beacon)
+        minX = math.min(sensor.x - d, minX)
+        maxX = math.max(sensor.x + d, maxX)
+      }
+    }
+    val y = 2_000_000
+    var impossible = 0L
+    (minX to maxX).foreach { x =>
+      val pt = aoc.Point(x, y)
+      if (!beacons.contains(pt)) {
+        if (sensorBeaconPairs.exists(p =>
+          pt.manhattanDistanceTo(p._1) <= p._1.manhattanDistanceTo(p._2)
+        )) {
+          impossible = impossible + 1
+        }
+      }
+    }
+    impossible
   }
 
-  override def part2(input: Array[String]): Any = "???"
+  val test =
+    """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+      |Sensor at x=9, y=16: closest beacon is at x=10, y=16
+      |Sensor at x=13, y=2: closest beacon is at x=15, y=3
+      |Sensor at x=12, y=14: closest beacon is at x=10, y=16
+      |Sensor at x=10, y=20: closest beacon is at x=10, y=16
+      |Sensor at x=14, y=17: closest beacon is at x=10, y=16
+      |Sensor at x=8, y=7: closest beacon is at x=2, y=10
+      |Sensor at x=2, y=0: closest beacon is at x=2, y=10
+      |Sensor at x=0, y=11: closest beacon is at x=2, y=10
+      |Sensor at x=20, y=14: closest beacon is at x=25, y=17
+      |Sensor at x=17, y=20: closest beacon is at x=21, y=22
+      |Sensor at x=16, y=7: closest beacon is at x=15, y=3
+      |Sensor at x=14, y=3: closest beacon is at x=15, y=3
+      |Sensor at x=20, y=1: closest beacon is at x=15, y=3""".stripMargin.split("\n")
+
+  override def part2(input: Array[String]): Any = {
+    // Based on https://github.com/MathCroc/aoc/blob/master/2022/day15/main.cpp
+    val coverage = mutable.Buffer[(aoc.Point, Int)]()
+    input.foreach {
+      case pLine(sX, sY, bX, bY) => {
+        val sensor = aoc.Point(sX.toInt, sY.toInt)
+        val beacon = aoc.Point(bX.toInt, bY.toInt)
+        val d = sensor.manhattanDistanceTo(beacon)
+        coverage += ((sensor, d))
+      }
+    }
+    breakable {
+      coverage.indices.foreach { i =>
+        (i + 1 until coverage.size).foreach { j =>
+          val (a, da) = coverage(i)
+          val (b, db) = coverage(j)
+          val distance = a.manhattanDistanceTo(b)
+          if (distance - 1 <= da + db) {
+            var p = intersection(1, -(a.x - da) + a.y, -1, (b.x - db) + b.y)
+            if (!checkCoverage(coverage, p.x - 1, p.y)) {
+              break
+            }
+            p = intersection(1, -(a.x - da) + a.y, -1, (b.x + db) + b.y)
+            if (!checkCoverage(coverage, p.x, p.y + 1)) {
+              break
+            }
+            p = intersection(1, -(a.x + da) + a.y, -1, (b.x - db) + b.y)
+            if (!checkCoverage(coverage, p.x, p.y - 1)) {
+              break
+            }
+            p = intersection(1, -(a.x + da) + a.y, -1, (b.x + db) + b.y)
+            if (!checkCoverage(coverage, p.x + 1, p.y)) {
+              break
+            }
+            p = intersection(-1, (a.x + da) + a.y, 1, -(b.x + db) + b.y)
+            if (!checkCoverage(coverage, p.x + 1, p.y)) {
+              break
+            }
+            p = intersection(-1, (a.x + da) + a.y, 1, -(b.x - db) + b.y);
+            if (!checkCoverage(coverage, p.x, p.y + 1)) {
+              break
+            }
+            p = intersection(-1, (a.x - da) + a.y, 1, -(b.x + db) + b.y);
+            if (!checkCoverage(coverage, p.x, p.y - 1)) {
+              break
+            }
+            p = intersection(-1, (a.x - da) + a.y, 1, -(b.x - db) + b.y);
+            if (!checkCoverage(coverage, p.x - 1, p.y)) {
+              break
+            }
+          }
+        }
+      }
+    }
+    ""
+  }
+
+  private[this] def intersection(ka: Int, a: Int, kb: Int, b: Int): aoc.Point = {
+    val x = (b - a) / (ka - kb)
+    aoc.Point(x, ka * x + a)
+  }
+
+  private[this] def checkCoverage(coverage: mutable.Buffer[(aoc.Point, Int)], x: Int, y: Int): Boolean = {
+    if (x < 0 || x > 4_000_000 || y < 0 || y > 4_000_000) {
+      true
+    } else {
+      val pt = aoc.Point(x, y)
+      val exists = coverage.exists { case (pos, d) =>
+        val distance = pt.manhattanDistanceTo(pos)
+        distance <= d
+      }
+      if (!exists) {
+        println(x * 4_000_000L + y)
+      }
+      exists
+    }
+  }
 }
 
 object Day15 {
   def apply() = new Day15
+
+  val pLine = raw"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)".r
 }
---
+++ src/main/scala/aoc2022/Day16.scala
@@ -0,0 +1,162 @@
+package aoc2022
+
+import scala.collection.mutable
+import Day16._
+
+import scala.util.control.Breaks.{break, breakable}
+
+class Day16 extends aoc.Day(2022, 16) {
+  override def part1(input: Array[String]): Any = {
+    val valves = mutable.Map[String, Valve]()
+    test.foreach {
+      case line(id: String, rate: String, neighbours: String) =>
+        val valve = new Valve(id, rate.toInt, neighbours.split(", ").toSet)
+        valves(id) = valve
+    }
+    val start = "AA"
+    val open = Set[String]()
+    search(valves.toMap, start, Set(), open, 30, 0)
+  }
+
+  val test =
+    """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+      |Valve BB has flow rate=13; tunnels lead to valves CC, AA
+      |Valve CC has flow rate=2; tunnels lead to valves DD, BB
+      |Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+      |Valve EE has flow rate=3; tunnels lead to valves FF, DD
+      |Valve FF has flow rate=0; tunnels lead to valves EE, GG
+      |Valve GG has flow rate=0; tunnels lead to valves FF, HH
+      |Valve HH has flow rate=22; tunnel leads to valve GG
+      |Valve II has flow rate=0; tunnels lead to valves AA, JJ
+      |Valve JJ has flow rate=21; tunnel leads to valve II""".stripMargin.split("\n")
+
+  var bestSoFar = Int.MinValue
+
+  private[this] def search(valves: Map[String, Valve], location: String, visited: Set[String], open: Set[String], timeRemaining: Int, currentFlow: Int): Int = {
+    if (timeRemaining == 0) {
+      if (currentFlow > bestSoFar) {
+        bestSoFar = currentFlow
+      }
+      currentFlow
+    } else if (!valves.keySet.diff(open).exists(id => valves(id).rate > 0)) {
+      // No more to open
+      val result = currentFlow + (open.map { o => valves(o).rate * timeRemaining }.sum)
+      if (result > bestSoFar) {
+        bestSoFar = result
+      }
+      result
+    } else {
+      val theoreticalMax = currentFlow + open.map(o =>
+        valves(o).rate * timeRemaining
+      ).sum + (valves(location).rate * timeRemaining) + (valves.keySet.diff(open) - location).map { couldBeOpened =>
+        valves(couldBeOpened).rate * (timeRemaining - 1)
+      }.sum
+      if (theoreticalMax < bestSoFar) {
+        0
+      } else {
+        val openScore = if (valves(location).rate > 0 && !open.contains(location)) {
+          // Spend one move opening valve in current location
+          search(
+            valves, location, Set(), open + location, timeRemaining - 1, currentFlow + open.map(id => valves(id).rate).sum)
+        } else {
+          Int.MinValue
+        }
+        // Move somewhere else
+        val valve = valves(location)
+        math.max(
+          openScore,
+          valve.neighbours.diff(visited).toSeq.sortBy(id => -valves(id).rate).map { neighbour =>
+            search(valves, neighbour, visited + location, open, timeRemaining - 1, currentFlow + open.map(id => valves(id).rate).sum)
+          }.maxOption.getOrElse(Int.MinValue)
+        )
+      }
+    }
+  }
+
+  override def part2(input: Array[String]): Any = {
+    val valves = mutable.Map[String, Valve]()
+    input.foreach {
+      case line(id: String, rate: String, neighbours: String) =>
+        val valve = new Valve(id, rate.toInt, neighbours.split(", ").toSet)
+        valves(id) = valve
+    }
+    doubleSearch(valves.toMap)
+  }
+
+
+  case class State(time: Int, locationA: String, locationB: String, currentFlow: Int, opened: Set[String])
+
+  private[this] def doubleSearch(valves: Map[String, Valve]): Int = {
+    val maxFlow = valves.values.map(_.rate).sum
+    val start = "AA"
+    val states = mutable.ArrayDeque[State](State(1, start, start, 0, Set()))
+    val seen = mutable.Map[(Int, String, String), Int]().withDefaultValue(Int.MinValue)
+    var doubleBestSoFar = Int.MinValue
+    while (states.nonEmpty) {
+      breakable {
+        val current = states.removeHead()
+        val time = current.time
+        val locationA = current.locationA
+        val locationB = current.locationB
+        val score = current.currentFlow
+        val opened = current.opened
+        val currentFlow = opened.map(id => valves(id).rate).sum
+
+        if (seen((time, locationA, locationB)) >= score) {
+          break()
+        }
+        seen((time, locationA, locationB)) = score
+
+        if (time == 26) {
+          if (score > doubleBestSoFar) {
+            doubleBestSoFar = score
+            println("New best: " + doubleBestSoFar)
+          }
+          break()
+        }
+
+        if (currentFlow == maxFlow) {
+          // No more to open
+          val result = score + (currentFlow * (26 - time))
+          states.append(State(26, locationA, locationB, result, opened))
+        } else {
+          // A can move or open
+          // B can move or open
+          val aCanOpen = valves(locationA).rate > 0 && !opened.contains(locationA)
+          val bCanOpen = valves(locationB).rate > 0 && !opened.contains(locationB)
+          val aMoves = valves(locationA).neighbours
+          val bMoves = valves(locationB).neighbours
+          val nextFlow = score + opened.map(id => valves(id).rate).sum
+          if (aCanOpen && bCanOpen && locationA != locationB) {
+            states.append(State(time + 1, locationA, locationB, nextFlow + valves(locationA).rate + valves(locationB).rate, opened + locationA + locationB))
+          } else if (aCanOpen) {
+            bMoves.toSeq.foreach { neighbourB =>
+              states.append(State(time + 1, locationA, neighbourB, nextFlow + valves(locationA).rate, opened + locationA))
+            }
+          } else if (bCanOpen) {
+            aMoves.toSeq.foreach { neighbourA =>
+              states.append(State(time + 1, neighbourA, locationB, nextFlow + valves(locationB).rate, opened + locationB))
+            }
+          }
+          // Just moves
+          aMoves.toSeq.foreach { neighbourA =>
+            bMoves.toSeq.foreach { neighbourB =>
+              states.append(State(time + 1, neighbourA, neighbourB, nextFlow, opened))
+            }
+          }
+        }
+      }
+    }
+    doubleBestSoFar
+  }
+}
+
+object Day16 {
+  def apply() = new Day16
+
+  val line = raw"Valve (.+?) has flow rate=(\d+); tunnels? leads? to valves? (.+)".r
+  //             Valve AA    has flow rate=0    ; tunnels lead to valves    DD, II, BB
+}
+
+class Valve(name: String, val rate: Int, val neighbours: Set[String]) {
+}
