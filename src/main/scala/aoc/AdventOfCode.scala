a8eb382beb5cbf81ed0283039e87238e4eb8e1db
Mike Cripps <mike.a.cripps@gmail.com>
2023-12-02 02:36:10 +0000

    Day2 prep

--- src/main/scala/aoc/AdventOfCode.scala
+++ src/main/scala/aoc/AdventOfCode.scala
@@ -35,7 +35,7 @@ object AdventOfCode extends App {
     println("Advent Of Code")
     println("--------------")
 
-    val days: Array[aoc.Day] = Array(Day1())
+    val days: Array[aoc.Day] = Array(Day2())
 
     val tracer = telemetry.getTracer("aoc")
     val compSpan = tracer.spanBuilder("adventofcode").startSpan()
---
+++ src/main/scala/aoc2023/Day2.scala
@@ -0,0 +1,11 @@
+package aoc2023
+
+class Day2 extends aoc.Day(2023, 2) {
+  override def part1(input: Array[String]): Any = "???"
+
+  override def part2(input: Array[String]): Any = "???"
+}
+
+object Day2 {
+  def apply() = new Day2
+}
